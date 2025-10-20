18fd5dd1b4db7a58c7e7b09910c66db5369408cd
amy <105334607@student.swin.edu.au>
2025-10-20 14:54:26 +1100

    Update jobs.php
    Changes code from html file into php file

--- project2/jobs.php
+++ project2/jobs.php
@@ -1,2 +1,18 @@
 <!-- jobs page, amy -->
- 
+ <?php
+ include 'header.inc';
+ ?>
+
+ <main id="jobs-main" class="jobs">
+    <a href="#jobs-main" class="visually-hidden">Skip to main content of this page</a>
+    <aside aria-labelledby="join-us">
+        <h2 id="join-us">Why should you considers joining us at SDLRC?</h2>
+        <p>
+                Our Careers help advance inclusive, industrial connected-learnings. We embed<strong> digital
+                literacies</strong> across curricula and explore new pedagogies with evidence-based practice.
+        </p>
+        <ul>
+                <li>Flexible, supportive team dynamic and cultures</li>
+                <li>Access to learning technologies and analytics sandboxes</li>
+                <li>Professional development and Industrial mentoring</li>
+        </ul>
