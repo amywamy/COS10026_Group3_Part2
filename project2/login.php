6d6e96b89656891f5d53c549b1bbae6b6a59cede
amy <105334607@student.swin.edu.au>
2025-10-27 00:12:24 +1100

    updates

--- project2/login.php
+++ project2/login.php
@@ -32,15 +32,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         <meta name="author" content="Nithya B">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login Page</title>
+        <link rel="stylesheet" href="../styles/style.css">
     </head>
     <body id="admin-login">
+        <div class="login-box">
+            <img src="../images/website_logo.png" alt="SDLRC Logo" class="logo">
         <h1>Login in</h1>
+        <p class="subtitle">Admin Portal access</p>
         <form method ="POST" action="login.php">
+            <div class="input-group">
             <label> Username:</label>
-            <input type="text" name="username" required>
+            <input type="text" name="username" required></div>
+            <div class="input-group">
             <label> Password:</label>
             <input type="password" name="password">
-            <button type="submit">Login</button>
+            <button type="submit">Login</button></div>
         </form>
     </body>
 </html>
